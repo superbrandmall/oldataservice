75179946ab088e74b88cf315051389a090104e39
bbjohn <295322187@qq.com>
2018-02-26 16:33:19 +0800

    商户使用新版同步

--- oldataservice/src/main/java/com/sbm/module/partner/hd/job/merchant/HdMerchantJob.java
+++ oldataservice/src/main/java/com/sbm/module/partner/hd/job/merchant/HdMerchantJob.java
@@ -1,20 +1,21 @@
 package com.sbm.module.partner.hd.job.merchant;
 
+import com.sbm.module.common.api.jobinteractive.job.BaseJob;
+import com.sbm.module.common.base.constant.CommonConstant;
+import com.sbm.module.onlineleasing.base.merchant.biz.ISyncMerchantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-import com.sbm.module.common.api.jobinteractive.job.BaseJob;
-import com.sbm.module.onlineleasing.base.merchant.biz.ISyncMerchantService;
-
 /*****************************************************************************/
+
 /**
  * Project Name:oldataservice<br/>
  * Package Name:com.sbm.module.partner.hd.job.sync.floor<br/>
  * File Name:FloorJob.java<br/>
- * 
+ * <p>
  * 作成日 ：2017-9-25 上午10:42:32 <br/>
- * 
+ *
  * @author ：junkai.zhang
  */
 @Component("hdMerchantJob")
@@ -25,8 +26,9 @@ public class HdMerchantJob extends BaseJob {
 	private ISyncMerchantService syncMerchantService;
 
 	protected void execute() {
+		CommonConstant.FRAMEWORK.info("商户使用新版同步");
 		// 同步商户
-		syncMerchantService.sync();
+//		syncMerchantService.sync();
 	}
 
 }
