1e73b068dd073197a7c815fb08ed53c7a0bf9950
bbjohn <295322187@qq.com>
2018-03-19 17:05:27 +0800

    旧系统取消权限验证部分

--- oldataservice/src/main/java/com/sbm/module/common/api/jsonwebtoken/interceptor/JSONWebTokenInterceptor.java
+++ oldataservice/src/main/java/com/sbm/module/common/api/jsonwebtoken/interceptor/JSONWebTokenInterceptor.java
@@ -35,31 +35,33 @@ public class JSONWebTokenInterceptor implements HandlerInterceptor {
 		response.setHeader(JSONWebTokenConstant.LOGIN, request.getHeader(JSONWebTokenConstant.LOGIN));
 		response.setHeader(JSONWebTokenConstant.AUTHORIZATION, request.getHeader(JSONWebTokenConstant.AUTHORIZATION));
 
-		if (handler instanceof HandlerMethod) {
-			// 判断方法上是否有
-			Authorization authorization = ((HandlerMethod) handler).getMethodAnnotation(Authorization.class);
-			// 如果方法上没有，判断类上是否有
-			if (null == authorization) {
-				authorization = ((HandlerMethod) handler).getBeanType().getAnnotation(Authorization.class);
-			}
+		// 旧系统取消api权限验证部分
 
-			// 声明，并且需要时才验证
-			if (authorization != null && authorization.validate()) {
-				int type = authorization.type();
-				// 获取login
-				String login = request.getHeader(JSONWebTokenConstant.LOGIN);
-				// 获取token
-				String token = request.getHeader(JSONWebTokenConstant.AUTHORIZATION);
-				// 校验对象
-				JSONWebToken jsonWebToken = new JSONWebToken(login, type, token);
-				// 验证逻辑
-				service.isValid(jsonWebToken);
-				// 验证成功刷新token
-				response.setHeader(JSONWebTokenConstant.AUTHORIZATION, service.getJWTString(jsonWebToken));
-				// 回传login
-				response.setHeader(JSONWebTokenConstant.LOGIN, login);
-			}
-		}
+//		if (handler instanceof HandlerMethod) {
+//			// 判断方法上是否有
+//			Authorization authorization = ((HandlerMethod) handler).getMethodAnnotation(Authorization.class);
+//			// 如果方法上没有，判断类上是否有
+//			if (null == authorization) {
+//				authorization = ((HandlerMethod) handler).getBeanType().getAnnotation(Authorization.class);
+//			}
+//
+//			// 声明，并且需要时才验证
+//			if (authorization != null && authorization.validate()) {
+//				int type = authorization.type();
+//				// 获取login
+//				String login = request.getHeader(JSONWebTokenConstant.LOGIN);
+//				// 获取token
+//				String token = request.getHeader(JSONWebTokenConstant.AUTHORIZATION);
+//				// 校验对象
+//				JSONWebToken jsonWebToken = new JSONWebToken(login, type, token);
+//				// 验证逻辑
+//				service.isValid(jsonWebToken);
+//				// 验证成功刷新token
+//				response.setHeader(JSONWebTokenConstant.AUTHORIZATION, service.getJWTString(jsonWebToken));
+//				// 回传login
+//				response.setHeader(JSONWebTokenConstant.LOGIN, login);
+//			}
+//		}
 		return true;
 	}
 
